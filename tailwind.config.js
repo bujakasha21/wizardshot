bae3f58a5b7b1d875a18905bb0ea92f3f1dbea0f
bujakasha21 <bujakdavor21@gmail.com>
2024-03-18 02:02:16 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        textColor: "#18181B",
+        buttonCol: "#18181C",
+        gradient1: "#AB96F8",
+        gradient2: "#FF99D0",
+        blueCol: "#0F172A",
+      },
+    },
+  },
+  plugins: [],
+};
